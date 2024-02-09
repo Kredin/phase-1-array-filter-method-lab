4f90a98aa55ea23219559a4cb1b207b095b17a3a
Blade <bladeprojectz@gmail.com>
2024-02-09 20:14:23 +0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+function findMatching(array, string) {
+    const filtered = array.filter(function (driver){
+        if(driver.toLowerCase() === string.toLowerCase()){
+            return driver
+        }
+    });
+    return filtered
+}
+
+
+function fuzzyMatch(array, string) {
+    const filtered = array.filter(function (driver){
+        if(driver[0, 1] === string[0, 1]){
+            return driver
+        }
+    })
+    return filtered
+}
+
+
+function matchName(array, string) {
+    const filtered = array.filter(function (driver){
+        if(driver.name === string){
+            return driver
+        }
+    })
+    return filtered
+}
+
+
